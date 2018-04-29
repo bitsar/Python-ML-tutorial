e8c495e0b1d0663f5d6dd4bb31b6e8f123f03a40
bitsar <nick.sarros@gmail.com>
2018-04-29 14:56:52 +1000

    cleaned up variables control via import

---
+++ variables.py
@@ -0,0 +1,11 @@
+# Variables and Switches
+# Vars
+predict = 0.1
+NaN = -99999
+# Switches
+stock = "WIKI/GOOGL"
+openPrice = 'Adj. Open'
+closePrice = 'Adj. Close'
+highPrice = 'Adj. High'
+lowPrice = 'Adj. Low'
+volumePrice = 'Adj. Volume'
