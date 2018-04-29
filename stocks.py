738454823113823d8e57e8e95d6bf5e80c61cce5
bitsar <nick.sarros@gmail.com>
2018-04-29 15:02:44 +1000

    cleaned up variables control via import

--- stocks.py
+++ stocks.py
@@ -4,35 +4,25 @@ import numpy
 from sklearn import preprocessing, model_selection, svm
 from sklearn.linear_model import LinearRegression
 
-# Variables and Switches
-# Vars
-predict = 0.1
-NaN = -99999
-# Switches
-stock = "WIKI/GOOGL"
-openPrice = 'Adj. Open'
-closePrice = 'Adj. Close'
-highPrice = 'Adj. High'
-lowPrice = 'Adj. Low'
-volumePrice = 'Adj. Volume'
+import variables as vars
 
 # Data set initialisation
-dataFrame = quandl.get(stock)
-dataFrame = dataFrame[[openPrice, highPrice, lowPrice, closePrice, volumePrice]]
-dataFrame['High-Low-Percent'] = (dataFrame[highPrice] - dataFrame[lowPrice]) / dataFrame[lowPrice] * 100.0
-dataFrame['Percent-Change'] = (dataFrame[closePrice] - dataFrame[openPrice]) / dataFrame[openPrice] * 100.0
-dataFrame = dataFrame[[closePrice, highPrice, 'Percent-Change', volumePrice]]
+dataFrame = quandl.get(vars.stock)
+dataFrame = dataFrame[[vars.openPrice, vars.highPrice, vars.lowPrice, vars.closePrice, vars.volumePrice]]
+dataFrame['High-Low-Percent'] = (dataFrame[vars.highPrice] - dataFrame[vars.lowPrice]) / dataFrame[vars.lowPrice] * 100.0
+dataFrame['Percent-Change'] = (dataFrame[vars.closePrice] - dataFrame[vars.openPrice]) / dataFrame[vars.openPrice] * 100.0
+dataFrame = dataFrame[[vars.closePrice, vars.highPrice, 'Percent-Change', vars.volumePrice]]
 print(dataFrame.head())
 
 # Learning Components
-forecastColumn = closePrice
+forecastColumn = vars.closePrice
 
 # Instruction to fill any NaN data in the dataframe with value=-99,999
-dataFrame.fillna(value=NaN, inplace=True)
+dataFrame.fillna(value=vars.NaN, inplace=True)
 
 # Forecast ignore 1% of the entire length of the dataframe
 # IE - if data is 100 days old, predict 1 day into future
-forecastIgnore = int(math.ceil(predict * len(dataFrame)))
+forecastIgnore = int(math.ceil(vars.predict * len(dataFrame)))
 dataFrame['label'] = dataFrame[forecastColumn].shift(-forecastIgnore)
 
 # Drop NaN data
